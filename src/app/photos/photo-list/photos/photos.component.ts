aa76c9316abd908902da7462c66ec25fb9990273
Leandro <leo.sena777@gmail.com>
2020-04-27 01:08:28 -0300

    dev

---
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,33 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { Photo } from '../../photo/photo.type';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.scss'],
+})
+export class PhotosComponent implements OnChanges {
+  @Input() photos: Photo[] = [];
+  rows: any[] = [];
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.photos) {
+      this.rows = this.groupColumns(this.photos);
+    }
+  }
+
+  groupColumns(photos: Photo[]) {
+    const newRows = [];
+    for (let i = 0; i < photos.length; i += 3) {
+      newRows.push(photos.slice(i, i + 3));
+    }
+    return newRows;
+  }
+}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
-import { PhotoComponent } from './photo/photo.component';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 
 @NgModule({
-  declarations: [PhotoComponent],
-  exports: [PhotoComponent]
+  imports: [
+    PhotoListModule,
+    PhotoFormModule,
+    PhotoModule,
+  ],
 })
 export class PhotosModule {}
